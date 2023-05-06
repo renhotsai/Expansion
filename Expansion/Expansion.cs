7edb1a57939bcb2f6a3cda49944ace9a10a7aeee
Jeremy <renhotsai@gmail.com>
2023-05-06 21:35:50 +0800

    add .gitignore

--- Expansion/Expansion.cs
+++ Expansion/Expansion.cs
@@ -19,4 +19,21 @@ public static class Expansion
         }
         return compareArray!.Contains(value,StringComparer.InvariantCultureIgnoreCase);
     }
+    
+    public static T ConvertStringToEnum<T>(string str) where T : struct
+    {
+        return Enum.TryParse(str, true, out T enumValue) ? enumValue : new T();
+    }
+    
+    public static string ConvertConstValueToName<T>(string str)
+    {
+        var typeName = string.Empty;
+        foreach (var fieldInfo in typeof(T).GetFields())
+        {
+            if (fieldInfo.GetValue(obj: null)!.ToString() != str) continue;
+            typeName = fieldInfo.Name;
+            break;
+        }
+        return typeName;
+    }
 }
